ca3935f1fc35cbdaddfddd4e5c235143dac69d35
DSN Official <78654701+DSNOfficial@users.noreply.github.com>
2025-06-29 15:00:28 +0700

    Update helper.js

--- src/config/helper.js
+++ src/config/helper.js
@@ -53,7 +53,7 @@ export const formatDateServer = (date) => {
 
 export const Config = {
 
-    base_url :"http://api.tsnh.online/api/",
+    base_url :"https://api.tsnh.online/api/",
     image_path :"http://localhost:81/project/tsnh/public_file/images/",
    // image_path:"",
     version: "",
